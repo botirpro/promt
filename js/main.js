cf24f3e653e88588883cec2bb031299571458e68
botirpro <b.unarov@yandex.ru>
2021-11-11 19:24:01 +0500

    -m

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var isIsmingiz=prompt("ismingiz");
+var isFamiliyangiz=prompt("Familiyangiz");
+var isSharifingizgiz=prompt("Sharifingizgiz");
+var isYoshgiz=prompt("Yoshgiz");
+var isManzilingiz=prompt("Manzilingiz");
+var isMalumotingiz=prompt("Malumotingiz");
+var isIlmiydarajangiz=prompt("Ilmiydarajangiz");
+var isYutuqlaringiz=prompt("Yutuqlaringiz");
+var isIshlaysizmi=prompt("Ishlaysizmi");
+var isUylanganmisiz=prompt("Uylanganmisiz");
+
+
+console.log("ismingiz", isIsmingiz);
+console.log("Familiyangiz",isFamiliyangiz);
+console.log("Sharifingizgiz",isSharifingizgiz);
+console.log("Yoshgiz",isYoshgiz);
+console.log("Manzilingiz",isManzilingiz);
+console.log("Malumotingiz",isMalumotingiz);
+console.log("Ilmiydarajangiz",isIlmiydarajangiz);
+console.log("Yutuqlaringiz",isYutuqlaringiz);
+console.log("Ishlaysizmi",isIshlaysizmi);
+console.log("Uylanganmisiz",isUylanganmisiz);
+
